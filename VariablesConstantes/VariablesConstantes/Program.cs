5c1bcdd7f8a0c451acadb4f4d351f47e1a4bb064
carlosed08 <carloseduardoesquivel@gmail.com>
2026-07-28 11:16:13 -0500

    Este es mi primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui escribo del codigo fuente
+            string nombres= "Carlos Eduardo";
+            string apellidos = "Esquivel Caicedo";
+            Console.WriteLine("Bienvenidos al curso de fundamentos de programacion, Este es el proceso de: " + nombres + " " + apellidos);
+            
+        }
+    }
+}
